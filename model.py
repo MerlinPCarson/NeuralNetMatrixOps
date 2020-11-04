fixed gradient checker, added learning rate

model.py
@@ -36,10 +36,10 @@ class ProgressBar(tqdm):
 
 class Model():
 
-    def __init__(self, dimensions, lr_rate, activation='sigmoid'):
+    def __init__(self, dimensions, lr, activation='sigmoid'):
 
         self.dimensions = dimensions
-        self.lr_rate = lr_rate
+        self.lr = lr
         self.activation = activation
         self.glorot_weight_init(self.dimensions)
         
@@ -50,7 +50,6 @@ class Model():
         self.weights = weights[0].flatten()
         for layer in range(1, len(weights)):
             self.weights = np.concatenate((self.weights, weights[layer].flatten()))
-
 
     def forward(self, data, labels, params, dimensions, activation='sigmoid'):
         """
@@ -98,9 +97,6 @@ class Model():
 
         return cost, a2
 
-    def backward(self):
-        pass
-
     def evaluate(self, X, y):
 
         # get model predictions
@@ -138,7 +134,8 @@ class Model():
             for mb in range(num_batches):
                 mb_start = mb * mb_size
                 mb_end = mb_start + mb_size
-                loss, _ = forward_backward_prop(X_train[mb_start:mb_end], y_train[mb_start:mb_end], self.weights, self.dimensions, self.activation)
+                loss, grad = forward_backward_prop(X_train[mb_start:mb_end], y_train[mb_start:mb_end], self.weights, self.dimensions, self.activation)
+                self.weights -= self.lr * grad
                 epoch_loss += loss
 
             # process validation examples 

q2_gradcheck.py
@@ -31,10 +31,16 @@ def gradcheck_naive(f, x):
 
         # calculate gradient using central difference scheme
         random.setstate(rndstate)
-        f1 = f(x[ix]-h)[0]
+        grad_plus = np.copy(x)
+        grad_plus[ix] += h
+        grad_minus = np.copy(x)
+        grad_minus[ix] -= h
+        f1 = f(grad_plus)[0]
         random.setstate(rndstate)
-        f2 = f(x[ix]+h)[0]
-        numgrad = (f2-f1)/(2*h)
+        f2 = f(grad_minus)[0]
+        #f2 = f(x[ix]+h)[0]
+        #numgrad = (f2[ix]-f1[ix])/(2*h)
+        numgrad = (f1-f2)/(2*h)
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))

train.py
@@ -6,12 +6,6 @@ import matplotlib.pyplot as plt
 from model import Model, one_hot_encode
 
 
-def one_hot_encode(labels):
-    num_labels = len(np.unique(labels))
-    encoding = np.eye(num_labels)[labels.astype('uint8')]
-    return encoding
-
-
 def load_SPECT_data(path):
     data = np.genfromtxt(path, delimiter=',')
 
@@ -58,9 +52,9 @@ def main():
     num_epochs = 100 
     paitience = 10 
     paitience = None 
-    lr_rate = 0.01
+    lr = 0.01
     mb_size = 16 
-    model = Model(dimensions, lr_rate, activation='relu')
+    model = Model(dimensions, lr, activation='sigmoid')
     history = model.fit(X_train, y_train, X_val, y_val, num_epochs, mb_size, paitience=paitience)
 
     # determine accuracies for data sets